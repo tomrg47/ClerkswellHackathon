5f616bb51cf4b36c0b510c611a6e319d1dbfa327
Thomas Arnett <31830579+tomrg47@users.noreply.github.com>
2025-11-28 12:13:07 +0000

    Create member api (#1)

---
+++ ClerkswellHackathon.Web/Models/ApiResponse.cs
@@ -0,0 +1,8 @@
+namespace ClerkswellHackathon.Web.Models;
+
+public class ApiResponse<T>
+{
+    public bool Success { get; set; }
+    public string? Message { get; set; }
+    public T? Data { get; set; }
+}
